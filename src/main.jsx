81f1eab9a4b76b7ccc886f25677c906291b5c23f
Eduardos13 <edik.kozelsky@gmail.com>
2024-07-19 21:46:25 -0400

    1

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "modern-normalize";
+import App from "./components/App/App";
+import "src/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    
+    <App />
   </>
 );
